86e1854f4964b812148426d0e9ddc2486bce887a
Vasu Goel <vasu.goel@rupyy.com>
2026-01-30 15:03:53 +0530

    Crud fix

---
+++ backend/app/crud.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(models.User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+    return user
